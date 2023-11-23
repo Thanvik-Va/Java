f8bf699b3fea4511fab98f45130f053249430861
M Vamshi krishna <110537633+Thanvik-Va@users.noreply.github.com>
2023-11-23 13:32:59 +0530

    Update Program.java

--- Test/src/com/test/Program.java
+++ Test/src/com/test/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Main class is created..!!");
 		System.out.println("The way you learn is the way you gain");
+		System.out.println("The way you learn is the way you gain");
 	}
 
 }
