a899bd4fc90c3cc005bb62666968d69277b79c96
M Vamshi krishna <110537633+Thanvik-Va@users.noreply.github.com>
2023-11-23 13:28:22 +0530

    Update Program.java
    Just added an extra println statement

--- Test/src/com/test/Program.java
+++ Test/src/com/test/Program.java
@@ -4,6 +4,7 @@ public class Program {
 	
 	public static void main(String[] args) {
 		System.out.println("Main class is created..!!");
+		System.out.println("The way you learn is the way you gain");
 	}
 
 }
